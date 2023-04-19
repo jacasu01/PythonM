374fe149ae3a8749da2fd4e2847777c0bc448e2a
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2023-04-19 13:23:00 +0200

    Update ejercicio15_distancia.py

--- Ejercicios/ejercicio15_distancia.py
+++ Ejercicios/ejercicio15_distancia.py
@@ -7,7 +7,8 @@ dia = input("Introduce un día de la semana:")
 #Si el día introducido es correcto --> OK
 #Si el día introducido no es correcto --> Mostrar el que más se parezca
 
-
+#AYUDA: Operador in
+#AYUDA: Método capitalize de la clase str
 """
 palabra1 = "gato"
 palabra2 = "gata"
